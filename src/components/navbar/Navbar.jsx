118856e68da99d71d59946babf95b013dca6ea41
Nicholas Markus <nicxzmiller2@gmail.com>
2022-05-12 21:55:05 +0200

    navbar css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import "./navbar.scss";
 import LanguageIcon from '@mui/icons-material/Language';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import ListIcon from '@mui/icons-material/List';
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='navbar'>
       <div className='wrapper'>
         <div className='search'>
           <input type="text" placeholder='Search...'/>
@@ -14,7 +19,21 @@ const Navbar = () => {
         <div className="items">
           <div className="item">
             <LanguageIcon/>
-            English
+          </div>
+          <div className="item">
+            <DarkModeIcon/>
+          </div>
+          <div className="item">
+            <FullscreenExitIcon/>
+          </div>
+          <div className="item">
+            <NotificationsNoneIcon/>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineIcon/>
+          </div>
+          <div className="item">
+            <ListIcon/>
           </div>
         </div>
       </div>
