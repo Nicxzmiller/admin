5be1a8a41c49c8ca8de92ee9476078f186692827
Nicholas Markus <nicxzmiller2@gmail.com>
2022-05-12 10:20:18 +0200

    folder structure and initialization

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import Home from "./pages/home/Home";
+import SignIn from "./pages/signin/SignIn"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      Home
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="signin" element={<SignIn />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
