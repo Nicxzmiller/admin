c928ca79a24a794ae4ddacae64da0d077263c0c1
Nicholas Markus <nicxzmiller2@gmail.com>
2022-05-12 15:47:22 +0200

    side menu

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
 import "./sidebar.scss"
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import GroupIcon from '@mui/icons-material/Group';
+import LogoutIcon from '@mui/icons-material/Logout';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SettingsIcon from '@mui/icons-material/Settings';
+import DescriptionIcon from '@mui/icons-material/Description';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 
 const Sidebar = () => {
   return (
@@ -8,19 +20,77 @@ const Sidebar = () => {
       <div className="top">
         <span className="logo">G-51 Admin</span>
       </div>
+      <hr />
 
       {/* center section of sidebar */}
       <div className="center">
         <ul>
-          <li><span>Dashboard</span></li>
-          <li><span>Dashboard</span></li>
-          <li><span>Dashboard</span></li>
-          <li><span>Dashboard</span></li>
+
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon"/>
+            <span>Dashboard</span>
+          </li>
+
+          <p className="title">LISTS</p>
+          <li>
+            <GroupIcon className="icon"/>
+            <span>Users</span>
+          </li>
+          <li>
+            <ShoppingBagIcon className="icon"/>
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCardIcon className="icon"/>
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon"/>
+            <span>Delivery</span>
+          </li>
+
+          <p className="title">USEFUL LINKS</p>
+          <li>
+          <QueryStatsIcon className="icon"/>
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon"/>
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICES</p>
+          <li>
+            <HealthAndSafetyIcon className="icon"/>
+            <span>Health</span>
+          </li>
+          <li>
+            <DescriptionIcon className="icon"/>
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className="icon"/>
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleIcon className="icon"/>
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className="icon"/>
+            <span>Logout</span>
+          </li>
         </ul>
       </div>
 
       {/* bottom section of sidebar */}
-      <div className="bottom">Color option</div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
   )
 }
