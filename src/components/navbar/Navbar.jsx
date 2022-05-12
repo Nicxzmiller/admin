5ae4bbf42d14cc2b70efc74347ca4cd5fc050ec7
Nicholas Markus <nicxzmiller2@gmail.com>
2022-05-12 20:52:41 +0200

    container

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
-import "./navbar.scss"
+import "./navbar.scss";
+import LanguageIcon from '@mui/icons-material/Language';
 
 const Navbar = () => {
   return (
@@ -9,6 +10,12 @@ const Navbar = () => {
         <div className='search'>
           <input type="text" placeholder='Search...'/>
           <SearchIcon/>
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageIcon/>
+            English
+          </div>
         </div>
       </div>
     </div>
