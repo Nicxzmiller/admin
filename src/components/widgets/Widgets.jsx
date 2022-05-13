e798c8f9b98c172da04c4ab94095628ad621ed17
Nicholas Markus <nicxzmiller2@gmail.com>
2022-05-13 11:54:35 +0200

    widget data

--- src/components/widgets/Widgets.jsx
+++ src/components/widgets/Widgets.jsx
@@ -2,8 +2,50 @@ import React from 'react'
 import "./widgets.scss"
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import PersonIcon from '@mui/icons-material/Person';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import PaidIcon from '@mui/icons-material/Paid';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 
-const Widgets = () => {
+const Widgets = ({type}) => {
+
+  let data;
+
+  switch(type){
+    case "user":
+      data={
+        title:"USERS",
+        isMoney:false,
+        link:"See all users",
+        icon: <PersonIcon className='icon'/>
+      };
+      break;
+      case "order":
+      data={
+        title:"ORDERS",
+        isMoney:false,
+        link:"See all orders",
+        icon: <CreditCardIcon className='icon'/>
+      };
+      break;
+      case "earning":
+      data={
+        title:"EARNINGS",
+        isMoney:true,
+        link:"View earnings",
+        icon: <PaidIcon className='icon'/>
+      };
+      break;
+      case "balance":
+      data={
+        title:"BALANCE",
+        isMoney:true,
+        link:"View balance info",
+        icon: <AccountBalanceWalletIcon className='icon'/>
+      };
+      break;
+      default:
+        break;
+  }
   return (
     <div className='widget'>
         <div className="left">
