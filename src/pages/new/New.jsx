257ce153f2cf4439ef78ed9fd10569964a4480c7
Nicholas Markus <nicxzmiller2@gmail.com>
2022-05-17 15:52:05 +0200

    new user

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
 import './New.scss'
 
 const New = () => {
   return (
-    <div>New</div>
+    <div className='new'>
+      <Sidebar/>
+      <div className="newContainer">
+        <Navbar/>
+        <div className="top">
+          <h1>Add New User</h1>
+        </div>
+        <div className="bottom">
+          <div className="leftContainer">
+            <img src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" alt="image" />
+          </div>
+          <div className="rightContainer">
+            <form action="">
+              <div className="formInput">
+                <label htmlFor="Username">Username</label>
+                <input type="text" placeholder='e.g Johnny' />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
